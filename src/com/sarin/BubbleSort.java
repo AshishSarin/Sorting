a98d994d47193dd32b4b992a8a5c0ee0c3cc96d9
ashish <asareen007@gmail.com>
2018-08-09 03:39:26 +0530

    Added bubble sorting algorithm

--- src/com/sarin/BubbleSort.java
+++ src/com/sarin/BubbleSort.java
@@ -1,5 +1,37 @@
 package com.sarin;
 
-public class BubbleSort {
+public class BubbleSort
+{
+    private int[] a;
+
+    public BubbleSort(int[] a)
+    {
+        this.a = a;
+    }
+
+
+    public int[] sort()
+    {
+        for(int i=0; i<a.length-1; i++)
+        {
+            for(int j=0; j<a.length-i-1; j++)
+            {
+                if(a[j] > a[j+1])
+                {
+                    // swap
+                    swap(j, j+1);
+
+                }
+            }
+        }
+        return a;
+    }
+
+    private void swap(int x, int y)
+    {
+        int temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
+    }
 
 }
--- src/com/sarin/Main.java
+++ src/com/sarin/Main.java
@@ -1,8 +1,27 @@
 package com.sarin;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        int[] unsortedArray = {45,-61, 89, 9, 12, 0, 23, 98, -54};
+        display("Unsorted", unsortedArray);
+
+        BubbleSort bubbleSortObj = new BubbleSort(unsortedArray);
+        int[] bubbleSortArray = bubbleSortObj.sort();
+        display("Bubble Sort", bubbleSortArray);
+
+
+    }
+
+    public static void display(String description, int[] elems)
+    {
+        System.out.print(description + " --> ");
+        for(int elem : elems)
+        {
+            System.out.print(elem + " ");
+        }
+        System.out.println();
     }
 }
